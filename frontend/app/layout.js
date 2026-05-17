0ca11a5778d53cba9a85b9de2a626ad49aac1548
anthonylied <thatfunnyasianboy@gmail.com>
2026-05-16 22:07:49 -0700

    Add files via upload

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -5,16 +5,12 @@ export const metadata = {
 
 import './globals.css';
 import Providers from './providers.js';
-import Chatbot from '../components/chatbot/Chatbot.jsx';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Providers>
-          {children}
-          <Chatbot />
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
